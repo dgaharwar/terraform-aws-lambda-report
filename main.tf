2d62f76c595794a2862c263b7cf776b51d92d839
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-11-11 12:27:23 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region = var.region
-  access_key              = var.AWS_ACCESS_KEY_ID
-  secret_key              = var.AWS_SECRET_ACCESS_KEY
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 data "aws_partition" "current" {}

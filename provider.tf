8a8fb040de32736b576339009d7c45f8cf062293
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-11-11 12:15:20 +0000

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,11 @@
+terraform {
+  #required_version = ">= v1.1.9"
+
+  required_providers {
+    aws = {
+      # source                = "hashicorp/aws"
+      # version               = ">= 4.9"
+      # configuration_aliases = [aws.lambda_region]
+    }
+  }
+}
